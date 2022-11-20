bd9c75ce8489c4a06663d37e75cfa8c35bca9c38
ABU BILAL <abu.bilal@jellyfishtechnologies.com>
2022-11-20 23:34:04 +0530

    DMRC Fare Calculator

--- index.js
+++ index.js
@@ -18,41 +18,51 @@
     // })
 
 
-    window.onload=function(){
+
     let output=document.getElementById("output");
     let a=document.getElementById("station_from");
     let b=document.getElementById("station_to");
     let card=document.getElementById("payment_method");
-    let button=document.getElementById("Calculate_Fare");
-
-    button.onclick=function(){
-let nos;
-if(a>b){
-    nos=a-b;
-}else{
-    nos=b-a;
-}
-let cost;
-if(nos==0 || nos<5){
-    cost=10;
-}else if(nos<9){
-    cost=20;
-}else if(nos<13){
-    cost=30;
-}else if(nos<17){
-    cost=40;
-}else if(nos<21){
-    cost=50;
-}else{
-    cost=60;
-}
-
-if(card==true){
-    cost=cost-(cost*10)/100;
-}
-
-console.log(cost);
-output.innerHTML=cost;
- }
-}
-
+    // let button=document.getElementById("Calculate_Fare");
+    var selected_a, selected_b, selected_payment;
+  
+    a.addEventListener('change', () => {
+        selected_a = a.value;
+    })
+    b.addEventListener('change', () => {
+        selected_b = b.value;
+    })
+    card.addEventListener('change', () => {
+        selected_payment = card.value
+    })
+    function onClick(){
+        const nos=Math.abs(selected_a-selected_b)
+        
+        // let nos;
+        // if(selected_a>selected_b){
+        //     nos=selected_a-selected_b;
+        // }else{
+        //     nos=selected_b-selected_a;
+        // }
+            let cost;
+        if(nos==0 || nos<5){
+            cost=10;
+        }else if(nos<9){
+            cost=20;
+        }else if(nos<13){
+            cost=30;
+        }else if(nos<17){
+            cost=40;
+        }else if(nos<21){
+            cost=50;
+        }else{
+            cost=60;
+        }
+        let ans;
+        if(selected_payment == 'card'){
+             cost=cost-(cost*10)/100;
+        }
+        
+        console.log(cost);
+        output.innerHTML=cost;
+    }
